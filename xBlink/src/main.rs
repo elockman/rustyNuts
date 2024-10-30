083ce1b2f244a0a58e5909a49aa0d32df6309517
elockman <eric.lockman@gmail.com>
2024-10-30 12:41:12 -0600

    bug fixes

--- xBlink/src/main.rs
+++ xBlink/src/main.rs
@@ -5,6 +5,6 @@ use tokio::time::Duration;
 
 #[tokio::main]
 async fn main() {
-    let gpio_blinker = GpioBlinker::init(128); // Initialize GPIO pin 128
-    gpio_blinker.task(500, 500, 10).await;    // Blink with 500ms on, 500ms off, for 10 blinks
+    let gpio_blinker = GpioBlinker::init(128); // Initialize GPIO pin 128 (CHANGE FOR YOUR APPLICATION)
+    gpio_blinker.task(2500, 2500, 10).await;    // Blink with 2500ms on, 2500ms off, for 10 blinks
 }
